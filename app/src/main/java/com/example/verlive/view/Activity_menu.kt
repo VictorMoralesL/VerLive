0353a3f9c28196249c3bb8f699241a36aa59543e
LyraZC <leo13zc@gmail.com>
2022-06-18 16:27:48 -0600

    Proyecto

---
+++ app/src/main/java/com/example/verlive/view/Activity_menu.kt
@@ -0,0 +1,12 @@
+package com.example.verlive.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.verlive.R
+
+class Activity_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
